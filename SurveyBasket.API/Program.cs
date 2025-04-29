df81dda496b129cb3e7f62215f63ca28198c0298
Mohamed El-sawah <melsawsh345@gmail.com>
2025-04-29 07:06:03 +0300

    Firist Clean System

--- SurveyBasket.API/Program.cs
+++ SurveyBasket.API/Program.cs
@@ -1,8 +1,5 @@
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.Extensions.DependencyInjection;
-using SurveyBasket.API;
-using SurveyBasket.API.Middileware;
-using SurveyBasket.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,17 +11,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
-builder.Services.AddTransient<IOperationTransiente, WindoesOscs>();
-builder.Services.AddScoped<IOperationScoped, WindoesOscs>();
-builder.Services.AddSingleton<IOperationSingelton, WindoesOscs>();
-
-
-//builder.Services.AddTransient<IOperationTransiente, MacOsServices>();
-//builder.Services.AddScoped<IOperationScoped, MacOsServices>();
-//builder.Services.AddSingleton<IOperationSingelton, MacOsServices>();
-
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -33,17 +19,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseCustomMiddleware();
-
-//app.Use(async (context, next) =>
-//{
-//    app.Logger.LogError("Proccessing request");
-
-//    await next(context);
-
-//    app.Logger.LogWarning("Proccessing responce");
-//});
 
 app.UseHttpsRedirection();
 
